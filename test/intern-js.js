4c857885ca7a6034702f131abb4a19ad3dc9de90
Vincent Ogloblinsky <vogloblinsky@sii.fr>
2015-02-05 13:24:27 +0100

    Interne config for js tests npm run commands

---
+++ test/intern-js.js
@@ -0,0 +1,61 @@
+// Learn more about configuring this file at <https://github.com/theintern/intern/wiki/Configuring-Intern>.
+// These default settings work OK for most people. The options that *must* be changed below are the
+// packages, suites, excludeInstrumentation, and (if you want functional tests) functionalSuites.
+define({
+	// The port on which the instrumenting proxy will listen
+	proxyPort: 9002,
+
+	// A fully qualified URL to the Intern proxy
+	proxyUrl: 'http://localhost:9002/',
+
+	// Default desired capabilities for all environments. Individual capabilities can be overridden by any of the
+	// specified browser environments in the `environments` array below as well. See
+	// https://code.google.com/p/selenium/wiki/DesiredCapabilities for standard Selenium capabilities and
+	// https://saucelabs.com/docs/additional-config#desired-capabilities for Sauce Labs capabilities.
+	// Note that the `build` capability will be filled in with the current commit ID from the Travis CI environment
+	// automatically
+	capabilities: {
+		'selenium-version': '2.41.0',
+		'idle-timeout': 30
+	},
+
+	environments: [
+		{
+			browserName: 'chrome'
+		}
+	],
+
+	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
+	maxConcurrency: 3,
+
+	tunnel: 'SauceLabsTunnel',
+
+	// The desired AMD loader to use when running unit tests (client.html/client.js). Omit to use the default Dojo
+	// loader
+	useLoader: {
+		'host-node': 'dojo/dojo',
+		'host-browser': 'node_modules/dojo/dojo.js'
+	},
+
+	// Configuration options for the module loader; any AMD configuration options supported by the specified AMD loader
+	// can be used here
+	loader: {
+		// Packages that should be registered with the loader in each testing environment
+		packages: [
+			{
+				name: 'angular', location: 'src/vendor/angular'
+			},
+			{
+				name: 'src', location: 'src/js'
+			}
+		]
+	},
+
+	// Non-functional test suite(s) to run in each browser
+	suites: [
+		'test/js/temperatureConverter'
+	],
+
+	// A regular expression matching URLs to files that should not be included in code coverage analysis
+	excludeInstrumentation: /^(?:test|src\/vendor|node_modules)\//
+});
---
+++ test/js/temperatureConverter.js
@@ -0,0 +1,20 @@
+/*global angular*/
+
+define([
+    'intern/chai!expect',
+    'intern!bdd',
+    'intern/order!src/vanilla-modules/temperatureConverter'
+], function(expect, bdd) {
+
+    bdd.describe('temperatureConverter module', function() {
+
+        bdd.it('should converte a celsius temperature to fahrenheit', function() {
+            expect(temperatureConverter.celsiusToFahrenheit(5)).to.equal(41);
+        });
+
+        bdd.it('should converte a fahrenheit temperature to celsius', function() {
+            expect(temperatureConverter.fahrenheitToCelsius(41)).to.equal(5);
+        });
+
+    });
+});

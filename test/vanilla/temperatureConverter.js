69b9cdc3981a25d676cc5585024d10dee29da88e
Vincent Ogloblinsky <vogloblinsky@sii.fr>
2015-02-09 16:04:10 +0100

    Fix syntax error

--- test/vanilla/temperatureConverter.js
+++ test/vanilla/temperatureConverter.js
@@ -6,11 +6,11 @@ define([
 
     bdd.describe('temperatureConverter module', function() {
 
-        bdd.it('should converte a celsius temperature to fahrenheit', function() {
+        bdd.it('should convert a celsius temperature to fahrenheit', function() {
             expect(temperatureConverter.celsiusToFahrenheit(5)).to.equal(41);
         });
 
-        bdd.it('should converte a fahrenheit temperature to celsius', function() {
+        bdd.it('should convert a fahrenheit temperature to celsius', function() {
             expect(temperatureConverter.fahrenheitToCelsius(41)).to.equal(5);
         });
 

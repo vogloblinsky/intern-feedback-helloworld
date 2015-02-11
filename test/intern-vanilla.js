d30d39f980478a8cb8f000283cbd416ad6da5494
Vincent Ogloblinsky <vogloblinsky@sii.fr>
2015-02-11 10:55:57 +0100

    Cleaning

--- test/intern-vanilla.js
+++ test/intern-vanilla.js
@@ -2,13 +2,6 @@
 // These default settings work OK for most people. The options that *must* be changed below are the
 // packages, suites, excludeInstrumentation, and (if you want functional tests) functionalSuites.
 define({
-	// The desired AMD loader to use when running unit tests (client.html/client.js). Omit to use the default Dojo
-	// loader
-	useLoader: {
-		'host-node': 'dojo/dojo',
-		'host-browser': 'node_modules/dojo/dojo.js'
-	},
-
 	// Configuration options for the module loader; any AMD configuration options supported by the specified AMD loader
 	// can be used here
 	loader: {
